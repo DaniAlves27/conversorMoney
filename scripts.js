4f86386e8fff14b15b2f33cad7f10039620619a9
DaniAlves27 <daniel.alves08@outlook.com>
2023-12-13 18:47:09 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const convertButton = document.querySelector(".convert-button")
 const selecionar = document.querySelector(".selecionar-moeda")
 function convertValores() {
-    const inputValor = parseFloat(document.querySelector(".input-valor").value);
+    const inputValor = (document.querySelector(".input-valor").value);
     const valor = document.querySelector(".valor")
     const valor1 = document.querySelector(".valor1")
     
@@ -39,7 +39,8 @@ function convertValores() {
         valor1.innerHTML = new Intl.NumberFormat("es-SV", { 
             style: "currency",
             currency: "BTC",
-        }).format(inputValor / bitcoinToday) + `(${(inputValor/bitcoinToday).toFixed(10)})`;
+
+        }).format(inputValor / bitcoinToday) 
       }
 
 
